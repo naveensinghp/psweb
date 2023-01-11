621d6d16beb094b01b2366a58f05c98b83a4a6be
NaveenSingh <naveensingh.mtp@gmail.com>
2023-01-11 22:59:35 +0530

    Endofline added

--- pages/maincontent.js
+++ pages/maincontent.js
@@ -4,7 +4,8 @@ import RecentBlogPreview from '../components/RecentBlogPreview/RecentBlogPreview
 import { BREAKPOINTS } from "../constant";
 import Link from 'next/link';
 import { Heading } from '../components/Heading/Heading';
-
+import Icon from 'react-icons-kit';
+import {arrowRight} from 'react-icons-kit/feather/arrowRight'
 
 
 export default function MainContent(){
@@ -23,10 +24,14 @@ export default function MainContent(){
                     <Paragraph>
                     The Goal of this blog is about to share the stuff I&apos;ve learned often and the biggest motivation for me at first was just to remember things for myself just want to make sure that I keep this as reference for myself.
                     </Paragraph>
-                    {/* <EndofLine></EndofLine> */}
+                    <EndofLine></EndofLine>
                     {/* <svg  style={{ margin: '70px auto' }} width="100" height="50"><path d="M 0,25 Q 5,30 10,25 Q 15,20 20,25 Q 25,30 30,25 Q 35,20 40,25 Q 45,30 50,25 Q 55,20 60,25 Q 65,30 70,25 Q 75,20 80,25 Q 85,30 90,25 Q 95,20 100,25" fill="none" stroke-linecap="round" stroke="#D500F9" stroke-width="2"></path>
                     </svg> */}
-                    <Heading>Recent Blog</Heading>
+                    {/* <Headings>
+                      <h2>Recent Blog</h2>
+                      <h6>View All &nbsp;&nbsp;<Icon size={22} color="white" icon={arrowRight} /> </h6>
+                      </Headings> */}
+                   <Heading>Recent Blog</Heading>
                     <RecentBlogPreview
                     title="My Rewind of 2022"
                     createdDate="Decemember 31th, 2022"
@@ -46,6 +51,13 @@ export default function MainContent(){
 }
 
 
+const ViewAll = styled.h6`
+  color: blue;
+`;
+const Headings = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const NameStyle = styled.h1`
 letter-spacing: 0.9px`;
@@ -53,9 +65,9 @@ letter-spacing: 0.9px`;
 
 const EndofLine = styled.div`
 border: 3px solid #ababab;
-border-radius: 12px;
+border-radius: 10px;
 margin: 70px auto;
-width: 50px;`;
+width: 80px;`;
 
 
 const OuterWrapper = styled.div`
